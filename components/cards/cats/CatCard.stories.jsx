5a799097a45729af015989112adcc21c113e78fa
kyleowen12345 <gakyleowen@gmail.com>
2022-06-04 14:01:05 +0800

    feat: catcard feature

---
+++ components/cards/cats/CatCard.stories.jsx
@@ -0,0 +1,19 @@
+import CatCard from './CatCard';
+import { mockCatCardProps } from './CatCard.mocks';
+
+export default {
+  title: 'cards/CatCard',
+  component: CatCard,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <CatCard {...args} />;
+
+export const Base = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
+Base.args = {
+  ...mockCatCardProps.base,
+};
